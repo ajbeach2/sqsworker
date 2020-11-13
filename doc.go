38d69a441707f694ace5f9a3854ed10c0246fcfc
Alex Beach <ajbeach2@gmail.com>
2020-11-12 20:11:52 -0500

    fix docs

--- doc.go
+++ doc.go
@@ -19,27 +19,24 @@
 // For example:
 //
 //	import (
-//	"context"
-//	"github.com/ajbeach2/sqsworker"
-//	"github.com/aws/aws-sdk-go/aws"
-//	"github.com/aws/aws-sdk-go/aws/session"
-//	"github.com/aws/aws-sdk-go/service/sns"
-//	"github.com/aws/aws-sdk-go/service/sqs"
-//	"strings"
-// )
-
+//		"context"
+//		"github.com/ajbeach2/sqsworker"
+//		"github.com/aws/aws-sdk-go/aws"
+//		"github.com/aws/aws-sdk-go/aws/session"
+//		"github.com/aws/aws-sdk-go/service/sns"
+//		"github.com/aws/aws-sdk-go/service/sqs"
+//		"strings"
+//	)
 //	type LowerCaseWorker struct {
 //	}
-
+//
 //	func (l *LowerCaseWorker) Process(ctx context.Context, m *sqs.Message, w *sns.PublishInput) error {
 //		*w.Message = strings.ToLower(*m.Body)
 //		return nil
 //	}
-
 //	func ExampleWorker() {
 //		lowerCaseWorker := &LowerCaseWorker{}
 //		sess := session.New(&aws.Config{Region: aws.String("us-east-1")})
-
 //		queueURL, _ := sqsworker.GetOrCreateQueue("In", sqs.New(sess))
 //		topicArn, _ := sqsworker.GetOrCreateTopic("Out", sns.New(sess))
 //
